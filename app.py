5a071bfa0004bdcd9979d1cdadb04367781ba497
Woojeong Kim <dnwjdrkdtks@naver.com>
2024-02-01 02:30:57 +0900

    우정 first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 # db setting
@@ -34,6 +35,31 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+# 로그인 라우트
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+
+        # 클라이언트로부터 전송된 사용자 이름 가져오기
+        id = request.form['id']
+
+        # 데이터베이스에서 사용자 찾기
+        user = Register.query.filter_by(id=id).first()
+
+    if user:
+        # 로그인 성공 & 세션에 사용자 ID 저장하기
+        db.session.add( Register(id=request.form['id'], password=request.form['password']) )
+        # db.session.commit()
+
+        return "로그인 성공!"
+    
+    else:
+        # 로그인 실패
+        return "로그인 실패. 사용자 아이디 또는 비밀번호가 잘못되었습니다."
+
+
 
 
 if __name__ == "__main__":
