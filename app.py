f5213badac088d5c5a0c17cf80fe1cc5a3cb3918
Juno <anint3647@gmail.com>
2024-01-31 16:33:41 +0900

    First Project

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# db setting
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    location_url = db.Column(db.String(10000), nullable=False)
+    image_url = db.Column(db.String(10000), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
