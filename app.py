9da0a96246ed4c2b16db2c8f363055a6c6e8f1f7
Woojeong Kim <dnwjdrkdtks@naver.com>
2024-02-01 12:21:46 +0900

    우정 second commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
@@ -32,35 +32,47 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if not session.get('login') :
+        return render_template('index.html')
+    else:
+        id = session.get('id')
+        return render_template('index.html', id=id)
+
 
 
 # 로그인 라우트
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
 
-        # 클라이언트로부터 전송된 사용자 이름 가져오기
+        # 사용자가 로그인 폼을 제출한 경우
+        # 클라이언트로부터 전송된 사용자 id, password 가져오기
         id = request.form['id']
+        password = request.form['password']
 
         # 데이터베이스에서 사용자 찾기
-        user = Register.query.filter_by(id=id).first()
+        user = Register.query.filter_by(id=id, password=password).first()
 
-    if user:
-        # 로그인 성공 & 세션에 사용자 ID 저장하기
-        db.session.add( Register(id=request.form['id'], password=request.form['password']) )
-        # db.session.commit()
-
-        return "로그인 성공!"
+        if user:
+            # 로그인 성공 & 세션에 사용자 ID 저장하기
+            session['login'] = True
+            session['id'] = user.id
+            return redirect(url_for('home'))
+        else:
+            # 로그인 실패
+            message = '로그인 실패. 사용자 아이디 또는 비밀번호가 잘못되었습니다.'
+            return render_template('login.html', message=message)
     
-    else:
-        # 로그인 실패
-        return "로그인 실패. 사용자 아이디 또는 비밀번호가 잘못되었습니다."
+    return render_template('login.html', message='로그인을 해주세요.')
 
-
+@app.route('/logout')
+def logout():
+    session['login'] = False
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
+    app.secret_key = "123123123"
     app.run(debug=True)
