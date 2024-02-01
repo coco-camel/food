65adebbd39a2aa16097576a4db6b6b0f3fe10f83
Juno <anint3647@gmail.com>
2024-02-01 18:41:05 +0900

    로그인 기능 구현 merge

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 app = Flask(__name__)
 
 # db setting
@@ -23,6 +23,7 @@ class Restaurant(db.Model):
     location_url = db.Column(db.String(10000), nullable=False)
     image_url = db.Column(db.String(10000), nullable=False)
 
+
 class Register (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(100), nullable=False)
@@ -33,27 +34,38 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if not session.get('login'):
+        return render_template('index.html')
+    else:
+        id = session.get('id')
+        return render_template('index.html', id=id)
+
 
 # 회원가입 페이지
+
+
 @app.route("/join")
 def join_screen():
-    return render_template("join.html");
+    return render_template("join.html")
 
 # 맛집 추천 페이지
+
+
 @app.route("/recommend")
 def recommend():
     return render_template("page.html")
 
 # 회원가입 로직
+
+
 @app.route("/user/regist", methods=['GET', 'POST'])
 def user_create():
     user = request.form.get("user")
     password = request.form.get("password")
 
-    user_exists = Register.query.filter_by(user = user).first()
+    user_exists = Register.query.filter_by(user=user).first()
     password_exists = Register.query.filter_by(password=password).first()
 
     if user_exists:
@@ -69,6 +81,8 @@ def user_create():
         return redirect(url_for("home"))
 
 # 게시글 작성
+
+
 @app.route("/main/list/write", methods=['GET', 'POST'])
 def recommend_restaurant():
     name = request.form.get("name")
@@ -77,14 +91,48 @@ def recommend_restaurant():
     description = request.form.get("description")
     location_url = request.form.get("location_url")
     image_url = request.form.get("image_url")
-    
+
     print(name, category, location, description, location_url, image_url)
 
-    recommend_list = Restaurant(name=name, category=category, location=location, description=description, location_url=location_url, image_url=image_url)
+    recommend_list = Restaurant(name=name, category=category, location=location,
+                                description=description, location_url=location_url, image_url=image_url)
     db.session.add(recommend_list)
     db.session.commit()
 
     return render_template("search.html")
 
+# 로그인 라우트
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        # 사용자가 로그인 폼을 제출한 경우
+        # 클라이언트로부터 전송된 사용자 id, password 가져오기
+        id = request.form['id']
+        password = request.form['password']
+        # 데이터베이스에서 사용자 찾기
+        user = Register.query.filter_by(user=id, password=password).first()
+        if user:
+            # 로그인 성공 & 세션에 사용자 ID 저장하기
+            session['login'] = True
+            session['id'] = user.id
+            return redirect(url_for('home'))
+        else:
+            # 로그인 실패
+            message = '로그인 실패. 사용자 아이디 또는 비밀번호가 잘못되었습니다.'
+            return render_template('login.html', message=message)
+
+    return render_template('login.html', message='로그인을 해주세요.')
+
+
+@app.route('/logout')
+def logout():
+    session['login'] = False
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
+    app.secret_key = "123123123"
     app.run(debug=True)
